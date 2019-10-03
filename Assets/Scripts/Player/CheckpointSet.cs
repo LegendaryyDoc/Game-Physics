ebb3c8d55043e24662d553563b8eba22db9a06bf
Nathan Knebusch <nateknebusch@gmail.com>
2019-10-02 17:23:21 -0700

    added it so you can die to objects with the death script on it, and got it so you can set a checkpoint by colliding with an object that has the script on it

---
+++ Assets/Scripts/Player/CheckpointSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointSet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.transform.CompareTag("Player"))
+        {
+            gameObject.GetComponent<PlayerAlive>().spawnPoint = this.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision collision)
+   {
+        if(collision.gameObject.transform.CompareTag("Player"))
+        {
+            Debug.Log("You Died");
+            collision.gameObject.GetComponent<PlayerAlive>().isAlive = false;
+        }
+   }
+}
